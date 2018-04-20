e7ec9e11420fe8920559156116ea6da51b4ea7f3
e.jager@amsterdam.nl <info@lytrix.com>
2018-04-20 11:50:29 +0200

    test subfolder static

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,26 +1,17 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-import HelloAgain from '@/components/HelloAgain'
 
 Vue.use(Router)
 
 export default new Router({
   mode: 'history',
+  base: process.env.ROUTER_BASE,
   routes: [
     {
       path: '/',
-      redirect: '/helloworld'
-    },
-    {
-      path: '/helloworld',
       name: 'HelloWorld',
       component: HelloWorld
-    },
-    {
-      path: '/helloagain/:text',
-      name: 'HelloAgain',
-      component: HelloAgain
     }
   ]
 })
